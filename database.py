fbec1bd8dcb1c21084fe59aa6e376f74ad779276
Abdul Rehman <ar4729189@gmail.com>
2026-08-29 16:48:06 +0500

    Update database.py

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
 # To read .env from DATABASE_URL 
-DATABASE_URL = os.getenv("DATABASE_URL", "......")
+DATABASE_URL = os.getenv("DATABASE_URL", "Your data base url")
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 # Database Session Factory
