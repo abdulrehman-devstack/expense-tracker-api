b8ea1082b01756fd9c1b5a56427ddc51dc6e8f6b
ar4729189-droid <ar4729189@gmail.com>
2026-09-15 17:12:57 +0500

    update js response to backend as of railway

--- auth.py
+++ auth.py
@@ -49,3 +49,25 @@ def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
 
     return new_user
+
+
+@router.post(
+    "/login",
+    response_model=schemas.UserResponse,
+    status_code=status.HTTP_200_OK,
+)
+def login_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, 
+            detail="Invalid Credentials (Email galat hai)"
+        )
+
+    if not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, 
+            detail="Invalid Credentials (Password galat hai)"
+        )
+
+    return db_user
